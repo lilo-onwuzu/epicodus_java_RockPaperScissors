a682fd806e194a7ea542fa6484d7bd8c277eaeb3
John Klein and Lilo Onwuzu <lilo@>
2016-04-20 09:05:43 -0700

    GREEN: scissors beat paper

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -2,7 +2,7 @@ public class RockPaperScissors {
 
   public Integer checkWinner (String playerOneMove, String playerTwoMove) {
 
-    if (playerOneMove.toLowerCase().equals("rock") && playerTwoMove.toLowerCase().equals("scissors") )  {
+    if ( (playerOneMove.toLowerCase().equals("rock") && playerTwoMove.toLowerCase().equals("scissors")) || (playerOneMove.toLowerCase().equals("scissors") && playerTwoMove.toLowerCase().equals("paper")) )  {
       return 1;
     } else {
       return 0;
