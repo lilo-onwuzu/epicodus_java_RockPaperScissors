945a9802f37f0a5572ff484172f7c8800a4b5691
John Klein and Lilo Onwuzu <lilo@>
2016-04-20 09:19:18 -0700

    GREEN for tie

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -4,14 +4,10 @@ public class RockPaperScissors {
 
     if ( (playerOneMove.toLowerCase().equals("rock") && playerTwoMove.toLowerCase().equals("scissors")) || (playerOneMove.toLowerCase().equals("scissors") && playerTwoMove.toLowerCase().equals("paper")) || (playerOneMove.toLowerCase().equals("paper") && playerTwoMove.toLowerCase().equals("rock")) )  {
       return 1;
-    // } else if (playerOneMove.toLowerCase().equals(playerTwoMove.toLowerCase())) {
-    //   return 0;
-    }
-
-    else {
+    } else if (playerOneMove.toLowerCase().equals(playerTwoMove.toLowerCase())) {
+      return 0;
+    } else {
       return 2;
     }
-
   }
-
 }
